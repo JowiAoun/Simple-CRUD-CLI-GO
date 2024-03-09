cf23674505464f9b1dd45c430d227fd3267a08ce
Jowi <83415433+JowiAoun@users.noreply.github.com>
2024-03-09 11:54:38 -0500

    Control functions

--- main.go
+++ main.go
@@ -1,23 +1,48 @@
 package main
 
-import "fmt"
+var (
+	choice          int
+	student_id      int
+	first_name      string
+	last_name       string
+	email           string
+	enrollment_date string
+	new_email       string
+	students        []Student
+)
+
+type Student struct {
+	Student_id int
+	First_name string
+	Last_name  string
+	Email      string
+	Enrolment  string
+}
 
 func main() {
-	fmt.Printf("Hi!!")
-}
+	DBConnect()
 
-func getAllStudents() {
+	for choice != 0 {
 
-}
+		PrintMenu(&choice)
 
-func addStudent(first_name string, last_name string, email string, enrollment_date string) {
-
-}
-
-func updateStudentEmail(student_id int, new_email string) {
-
-}
-
-func deleteStudent(student_id int) {
-
+		switch choice {
+		case 0:
+			DBClose()
+			return
+		case 1:
+			PrintOption1()
+			GetAllStudents(&students)
+			PrintStudents(&students)
+		case 2:
+			PrintOption2(&first_name, &last_name, &email, &enrollment_date)
+			AddStudent(first_name, last_name, email, enrollment_date)
+		case 3:
+			PrintOption3(&student_id, &new_email)
+			UpdateStudentEmail(student_id, new_email)
+		case 4:
+			PrintOption4(&student_id)
+			DeleteStudent(student_id)
+		}
+	}
 }
