bc1a2ea784ac561ead60e7cbc5791f914536e837
Jowi <83415433+JowiAoun@users.noreply.github.com>
2024-03-09 10:05:36 -0500

    Function prototypes

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Printf("Hi!!")
+}
+
+func getAllStudents() {
+
+}
+
+func addStudent(first_name string, last_name string, email string, enrollment_date string) {
+
+}
+
+func updateStudentEmail(student_id int, new_email string) {
+
+}
+
+func deleteStudent(student_id int) {
+
+}
